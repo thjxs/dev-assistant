9b24f0929a6ae14e5500729589ad706d4bba38c3
thjxs <tanhongjianx@163.com>
2019-03-21 10:39:16 +0000

    Add folder icon

---
+++ src/components/BaseFolderIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon">
+    <path d="M1 4 v12 a2 2 0 0 0 2 2 h14 a2 2 0 0 0 2 -2 v-11 a 2 2 0 0 0 -2 -2 h-7 a 8 4 0 0 0 -3 -1 h-3 a 10 4 0 0 0 -3 2z"/>
+ </svg>
+</template>
---
+++ src/components/BaseMDFileIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon">
+     <path d="M10 10 m-8 0 l6 -6 1 1 -6 6 6 6 -1 1 -7 -7z M10 10 m8 0 -6 -6 -1 1 6 6 -6 6 1 1 7 -7z" />
+  </svg>
+</template>
